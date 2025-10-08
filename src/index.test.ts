68827630a7cc534138552fb6c19a9d7d6fa0124f
sya-ri <git@s7a.dev>
2025-10-08 12:37:01 +0900

    refactor: Split `tx` object into separate schema function exports

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect } from "vitest";
-import { tx } from "./index";
+import * as tx from "./index";
 import {
     BooleanAttributeSchema,
     BooleanElementSchema,
--- src/index.ts
+++ src/index.ts
@@ -10,51 +10,64 @@ import {
     StringElementSchema,
 } from "./schema";
 
+export * from "./error";
+export {
+    type NodeLike,
+    type ParseOptions,
+    parse,
+    parseStream,
+} from "./parser";
+export type {
+    OptionalSchema,
+    RootableSchema,
+    Schema,
+} from "./schema";
+
 export type Kind = "attribute" | "element";
 
-export const tx = {
-    string: <Optional extends boolean = false>(
-        kind: Kind,
-        name: string,
-        optional: Optional = false as Optional,
-    ): Optional extends true ? OptionalSchema<string> : Schema<string> => {
-        const base =
-            kind === "attribute"
-                ? new StringAttributeSchema(name)
-                : new StringElementSchema(name);
-        return optional
-            ? // @ts-expect-error Returns OptionalSchema<string> when Optional = true
-              base
-            : new RequiredSchema(base);
-    },
-    number: <Optional extends boolean = false>(
-        kind: Kind,
-        name: string,
-        optional: Optional = false as Optional,
-    ): Optional extends true ? OptionalSchema<number> : Schema<number> => {
-        const base =
-            kind === "attribute"
-                ? new NumberAttributeSchema(name)
-                : new NumberElementSchema(name);
-        return optional
-            ? // @ts-expect-error Returns OptionalSchema<number> when Optional = true
-              base
-            : new RequiredSchema(base);
-    },
-    boolean: <Optional extends boolean = false>(
-        kind: Kind,
-        name: string,
-        optional: Optional = false as Optional,
-    ): Optional extends true ? OptionalSchema<boolean> : Schema<boolean> => {
-        const base =
-            kind === "attribute"
-                ? new BooleanAttributeSchema(name)
-                : new BooleanElementSchema(name);
-        return optional
-            ? // @ts-expect-error Returns OptionalSchema<boolean> when Optional = true
-              base
-            : new RequiredSchema(base);
-    },
+export const string = <Optional extends boolean = false>(
+    kind: Kind,
+    name: string,
+    optional: Optional = false as Optional,
+): Optional extends true ? OptionalSchema<string> : Schema<string> => {
+    const base =
+        kind === "attribute"
+            ? new StringAttributeSchema(name)
+            : new StringElementSchema(name);
+    return optional
+        ? // @ts-expect-error Returns OptionalSchema<string> when Optional = true
+          base
+        : new RequiredSchema(base);
+};
+
+export const number = <Optional extends boolean = false>(
+    kind: Kind,
+    name: string,
+    optional: Optional = false as Optional,
+): Optional extends true ? OptionalSchema<number> : Schema<number> => {
+    const base =
+        kind === "attribute"
+            ? new NumberAttributeSchema(name)
+            : new NumberElementSchema(name);
+    return optional
+        ? // @ts-expect-error Returns OptionalSchema<number> when Optional = true
+          base
+        : new RequiredSchema(base);
+};
+
+export const boolean = <Optional extends boolean = false>(
+    kind: Kind,
+    name: string,
+    optional: Optional = false as Optional,
+): Optional extends true ? OptionalSchema<boolean> : Schema<boolean> => {
+    const base =
+        kind === "attribute"
+            ? new BooleanAttributeSchema(name)
+            : new BooleanElementSchema(name);
+    return optional
+        ? // @ts-expect-error Returns OptionalSchema<boolean> when Optional = true
+          base
+        : new RequiredSchema(base);
 };
 
 export * from "./error";
